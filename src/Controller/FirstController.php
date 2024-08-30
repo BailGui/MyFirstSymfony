6627431b401ec1d5514f3ea82ceb0309a9ecd951
Guillaume <guillaumebaillieux88@gmail.com>
2024-08-30 15:19:57 +0200

    Fcontroller

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -13,7 +13,7 @@ class FirstController extends AbstractController
     public function index(): Response
     {
         return new Response(content:"<html><body><h1>Homepage</h1>
-        <nav><a href='./'>Accueil</a> | <a href='./json' target='_blank'>json</a></body></html>");
+        <nav><a href='./'>Accueil</a> | <a href='./json' target='_blank'>json</a> | <a href='./contact' target='_blank'>Contact</a></body></html>");
     }
     // attributs devenus inutiles #[Route('/json', name: 'app_first')]
     public function myJson(): JsonResponse
@@ -22,5 +22,10 @@ class FirstController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/FirstController.php',
         ]);
+    }
+
+    public function form(): Response
+    {
+        return new Response(content:"<html><body><h1>Contact</h1><nav><a href='./'>Accueil</a> | <a href='./json' target='_blank'>json</a> | <a href='./contact' target='_blank'>Contact</a></body></html>");
     }
 }
