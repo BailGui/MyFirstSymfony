1f15c583cc112c2e5100b62ffa09c5c3fea1c78c
Guillaume <guillaumebaillieux88@gmail.com>
2024-08-30 14:43:45 +0200

    FcontrollerRoutesYaml

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -4,10 +4,16 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 # use Symfony\Component\Routing\Attribute\Route;
 
 class FirstController extends AbstractController
 {
+    // création de la méthode pour notre page d'accueil
+    public function index(): Response
+    {
+        return new Response(content:"<html><body><h1>Hello</h1></body></html>");
+    }
     // attributs devenus inutiles #[Route('/json', name: 'app_first')]
     public function myJson(): JsonResponse
     {
